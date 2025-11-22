d5fe0739b9dfde8faa72448f9f32866ec4729215
PedroFavaro8 <favaroopedro@gmail.com>
2025-11-21 21:34:23 -0300

    Add files via upload

---
+++ Windows 7 Activator.bat
@@ -0,0 +1,90 @@
+@echo off
+TITLE Windows 7 Activator
+echo ---------------------------
+echo - Created by pedro
+echo - Windows 7 Activator
+echo - More : https://github.com/PedroFavaro8
+echo - (C) Copyright this program is made by me and don't have copyright.
+echo - This program requires administrator privileges. Please, if you found a way to run it without privileges, run this program as an administrator!
+echo ----------------------------
+echo - Supported Operating System
+echo - Windows 7 Ultimate
+echo - Windows 7 Professional
+echo - Windows 7 Home Premium
+echo - Windows 7 pro
+echo -Warning: the Windows 7 Pro activation might not work
+echo ----------------------------
+echo 1 ) Windows 7 Ultimate
+echo 2 ) Windows 7 Professional
+echo 3 ) Windows 7 Home Premium
+echo 4 ) Windows 7 pro
+
+SET /P M=Type : 
+
+IF %M%==1 GOTO WIN7U
+IF %M%==2 GOTO WIN7P
+IF %M%==3 GOTO WIN7H
+IF %M%==4 GOTO WIN71
+
+
+:WIN7U
+cls
+echo #######################
+echo # Activating Windows
+echo # Please Wait...
+echo #######################
+echo Activating Windows 7 Ultimate
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk H4Q8J-YXPPP-6X6GC-CRKM8-H9VPB >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk FG9VC-TY47G-BKVWC-R4T8P-Y86J9 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk YM8MJ-JKTGM-8GFQ6-X499R-FT7V7 >nul
+echo Thanks you using Windows 7 Activator. More https://github.com/PedroFavaro8
+echo Completed ! Please Restart Your Computer!
+Pause
+goto END
+
+:WIN7H
+cls
+echo #######################
+echo # Activating Windows
+echo # Please Wait...
+echo #######################
+echo Activating Windows 7 Home Premium
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 7X6JT-3FVCK-CBG9K-VQY8W-H64Y8 >nul
+echo Thanks you using Windows 7 Activator. More https://github.com/PedroFavaro8
+echo Completed ! Please Restart Your Computer!
+Pause
+goto END
+
+:WIN7P
+cls
+echo #######################
+echo # Activating Windows
+echo # Please Wait...
+echo #######################
+echo Activating Windows 7 Professional
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk H3P6D-CH2FV-H48D3-W6KBQ-39Q4J >nul
+echo Thanks you using Windows 7 Activator. More https://github.com/PedroFavaro8
+echo Completed ! Please Restart Your Computer!
+Pause
+goto END
+
+:WIN71
+cls
+echo #######################
+echo # Activating Windows
+echo # Please Wait...
+echo #######################
+echo Activating Windows 7 Professional
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 26FMV-TXQM6-6244R-4FVCM-YYD79 >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2MK2M-W7377-XKM7K-DK9PF-CRVVQ >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2RKR3-4322Q-XCV4K-JGDRX-BMW4F >nul
+cscript //nologo c:\windows\system32\slmgr.vbs /ipk 2TJ3R-4FXQ4-3RR9K-WG9P7-28D6W >nul
+echo Thanks you using Windows 7 Activator. More https://github.com/PedroFavaro8
+echo Completed ! Please Restart Your Computer!
+Pause
+goto END
+
+:END
+@echo on
+
+
